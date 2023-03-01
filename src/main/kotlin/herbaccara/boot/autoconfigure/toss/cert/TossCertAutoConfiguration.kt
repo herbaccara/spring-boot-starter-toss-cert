4eee4dcffb7632018df01076ff91b131d99bb71a
allbegray <allbegray@nate.com>
2023-03-01 16:34:05 +0900

    :bookmark: 0.0.2

--- src/main/kotlin/herbaccara/boot/autoconfigure/toss/cert/TossCertAutoConfiguration.kt
+++ src/main/kotlin/herbaccara/boot/autoconfigure/toss/cert/TossCertAutoConfiguration.kt
@@ -1,12 +1,10 @@
 package herbaccara.boot.autoconfigure.toss.cert
 
-import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import herbaccara.toss.cert.TossCertService
-import herbaccara.toss.cert.store.TossCertInMemoryTokenStore
-import herbaccara.toss.cert.store.TossCertTokenStore
-import org.springframework.beans.factory.annotation.Qualifier
+import herbaccara.toss.cert.model.store.TossCertInMemoryTokenStore
+import herbaccara.toss.cert.model.store.TossCertTokenStore
 import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
@@ -16,7 +14,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.http.converter.StringHttpMessageConverter
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.http.converter.support.AllEncompassingFormHttpMessageConverter
-import org.springframework.web.client.RestTemplate
 import java.nio.charset.StandardCharsets
 import java.util.*
 
@@ -25,21 +22,32 @@ import java.util.*
 @ConditionalOnProperty(prefix = "toss.cert", value = ["enabled"], havingValue = "true")
 class TossCertAutoConfiguration {
 
-    @Bean("tossCertObjectMapper")
-    fun objectMapper(properties: TossCertProperties): ObjectMapper {
+    @Bean
+    @ConditionalOnMissingBean
+    fun objectMapper(): ObjectMapper {
         return jacksonObjectMapper().apply {
             findAndRegisterModules()
-            configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, properties.failOnUnknownProperties)
         }
     }
 
-    @Bean("tossCertRestTemplate")
-    fun restTemplate(
-        @Qualifier("tossCertObjectMapper") objectMapper: ObjectMapper,
+    @Bean
+    @ConditionalOnMissingBean
+    fun tossCertTokenStore(): TossCertTokenStore {
+        return TossCertInMemoryTokenStore()
+    }
+
+    @Bean
+    fun tossCertService(
+        objectMapper: ObjectMapper,
         customizers: List<TossCertRestTemplateBuilderCustomizer>,
-        interceptors: List<TossCertClientHttpRequestInterceptor>
-    ): RestTemplate {
-        return RestTemplateBuilder()
+        interceptors: List<TossCertClientHttpRequestInterceptor>,
+        properties: TossCertProperties,
+        tokenStore: TossCertTokenStore
+    ): TossCertService {
+        if (properties.clientId.isEmpty()) throw NullPointerException()
+        if (properties.clientSecret.isEmpty()) throw NullPointerException()
+
+        val restTemplate = RestTemplateBuilder()
             .additionalInterceptors(*interceptors.toTypedArray())
             .messageConverters(
                 StringHttpMessageConverter(StandardCharsets.UTF_8),
@@ -52,23 +60,6 @@ class TossCertAutoConfiguration {
                 }
             }
             .build()
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(TossCertTokenStore::class)
-    fun tossCertTokenStore(): TossCertTokenStore {
-        return TossCertInMemoryTokenStore()
-    }
-
-    @Bean
-    fun tossCertService(
-        @Qualifier("tossCertRestTemplate") restTemplate: RestTemplate,
-        @Qualifier("tossCertObjectMapper") objectMapper: ObjectMapper,
-        properties: TossCertProperties,
-        tokenStore: TossCertTokenStore
-    ): TossCertService {
-        if (properties.clientId.isEmpty()) throw NullPointerException()
-        if (properties.clientSecret.isEmpty()) throw NullPointerException()
 
         return TossCertService(restTemplate, objectMapper, properties, tokenStore)
     }
